ccd0ef5a6ca99f78650ae341ccd23a1d69d709b9
Kalvin Pearce <kalvinpearce@gmail.com>
2020-05-09 22:28:57 +1000

    Fix naming

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Food Delivery UI',
+      title: 'Practice: Food Delivery',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[50],
